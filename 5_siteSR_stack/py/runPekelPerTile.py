24a4fb45f2346ba2a8d3a36c37ca98aaaa15ee12
B Steele <B.Steele@colostate.edu>
2024-09-08 11:26:36 -0600

    pull over pekel instance code from MR
    Co-authored-by: Matthew R.V. Ross <matthewross07@users.noreply.github.com>

--- 5_siteSR_stack/py/runPekelPerTile.py
+++ 5_siteSR_stack/py/runPekelPerTile.py
@@ -76,5 +76,44 @@ pekel = (ee.Image("JRC/GSW1_4/GlobalSurfaceWater")
   .select("occurrence"))
 
 # pull using V1 code from MR
+def waterfunc(buf):
+  #Define a 200m buffer around each point
+  invBuf = buf.buffer(200).geometry()
+  #Clip the pekel mask to this buffer
+  pekclip = occ.clip(invBuf)
+  #Reduce the buffer to pekel min and max
+  pekMin = pekclip.reduceRegion(ee.Reducer.minMax(), invBuf, 30)
+  #Add another reducer to get the median pekel occurnce
+  pekMed = pekclip.reduceRegion(ee.Reducer.median(),invBuf,30)
+  #Define the output features
+  out = buf.set({'max':pekMin.get('occurrence_max')})\
+          .set({'min':pekMin.get('occurrence_min')})\
+          .set({'med':pekMed.get('occurrence')})
+          
+  return out
+
+#Source function to limit number of tasks sent up to earth engine.
+exec(open("2_rsdata/src/GEE_pull_functions.py").read())    
+#Loop over the index stored in split
+for x in range(0,len(splitStart)):
+#turn our inventory into a feature collection by assigning lat longs and a site id.
+#This is done via list comprehension which is similar to a for loop but faster and
+#plays nice with earth engine.  Collections are limited for 5000 to avoid time outs
+#on the server side.
+  invOut = ee.FeatureCollection([ee.Feature(
+    ee.Geometry.Point([inv['long'][i], inv['lat'][i]]),
+    {'SiteID':inv['SiteID'][i]}) for i in range(splitStart[x], splitEnd[x])]) 
+  #Map this function over the 5000 or so created sites
+  outdata = invOut.map(waterfunc)
+  #Define a data export 
+  dataOut = ee.batch.Export.table.toDrive(collection = outdata,
+                                          description = "LandsatSitePull" + str(x),
+                                          folder='tempSiteWater',
+                                          fileFormat = 'csv')
+  #Send next task.
+  dataOut.start()
+#Make sure all Earth engine tasks are completed prior to moving on.  
+maximum_no_of_tasks(1,60)
+print('done')
 
 
